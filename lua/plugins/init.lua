8f76eb210e34f83fa60912521dffea91eeb29fc6
Shivam583-hue <shivamshivamshivam456@gmail.com>
2026-06-08 23:19:05 +0600

    removed avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,6 +10,11 @@ local plugins = {
     end,
   },
   {
+   'kaarmu/typst.vim',
+   ft = 'typst',
+   lazy=false,
+  },
+  {
     "NvChad/base46",
     lazy = true,
   },
@@ -58,43 +63,6 @@ local plugins = {
           horizontal = { mirror = false },
           vertical = { mirror = false },
         },
-      },
-    },
-  },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-    },
-    build = "make",
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-tree/nvim-web-devicons",
-      "zbirenbaum/copilot.lua",
-      {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = { insert_mode = true },
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
       },
     },
   },
