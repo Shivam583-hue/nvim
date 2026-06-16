6816112b60e7749585d601f8ee7332791a3a2f8f
Shivam583-hue <shivamshivamshivam456@gmail.com>
2026-06-17 00:17:39 +0600

    changes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -325,7 +325,11 @@ local plugins = {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        char = { enabled = false },
+      },
+    },
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
